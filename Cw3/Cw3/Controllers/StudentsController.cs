11baa4a007a415cac04dc27a5e9de18c209dc614
Grzegorz Fraczek <s17476@pjwstk.edu.pl>
2020-12-04 22:23:31 +0100

    Cw3 OK

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Cw3.Models;
+using Cw3.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService service)
+        {
+            _dbService = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to db
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukończone");
+        }
+    }
+}
---
+++ Cw3/Cw3/DAL/MockDbService.cs
@@ -0,0 +1,26 @@
+﻿using Cw3.Models;
+
+using System.Collections.Generic;
+
+
+namespace Cw3.Services
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+            new Student{IdStudent=1, FirstName="jan", LastName="Kowalski", IndexNumber="s12122"},
+            new Student{IdStudent=2, FirstName="Janina", LastName="Kowcz", IndexNumber="s12442"},
+            new Student{IdStudent=3, FirstName="Adam", LastName="Adamczewski", IndexNumber="s15552"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
